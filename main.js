319dd33bc15c245d4cd00a08745e896291c3018b
abduallahelbialy <abduallahelbialy@gmail.com>
2023-10-01 11:34:31 +0300

    project three jss

--- main.js
+++ main.js
@@ -8,3 +8,26 @@ window.onscroll = function () {
     });
   }
 };
+// the End of the years
+let countdowndata = new Date("Dec 31,2023 23:59:59").getTime();
+let counter = setInterval(() => {
+  //get data time now
+  let datanow = new Date().getTime();
+  //find the data difference between now and countdown data
+  let datadeif = countdowndata - datanow;
+  //get time unites
+  // let days=Math.floor(datadeif/1000/60/60/24)
+  let days = Math.floor(datadeif / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((datadeif % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((datadeif % (1000 * 60 * 60)) / (1000 * 60));
+  let second = Math.floor((datadeif % (1000 * 60)) / 1000);
+  document.querySelector(".days").innerHTML = days;
+  document.querySelector(".hours").innerHTML = hours;
+  document.querySelector(".minutes").innerHTML =
+    minutes < 10 ? `0${minutes}` : minutes;
+  document.querySelector(".seconds").innerHTML =
+    second < 10 ? `0${second}` : second;
+  if (datadeif < 0) {
+    clearInterval(counter);
+  }
+}, 1000);
